f992d7b37f688564ec5f58c7b69f477c3e5646ab
cappochinho <edwardprempeh18@gmail.com>
2023-01-13 23:06:53 +0000

    Testing start on all available servers

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """ Returns a welcome message to 0.0.0.0:5000"""
-    return "<p> Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
