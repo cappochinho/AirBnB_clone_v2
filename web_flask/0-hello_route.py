6c718f654ff1cd8a7dbf82ff0f129719ad4ba217
cappochinho <edwardprempeh18@gmail.com>
2023-01-13 23:12:29 +0000

    Made init file executable

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-    Script that starts a Flask web app on 0.0.0.0:5000
-"""
+
+""" Script that starts a Flask web app on 0.0.0.0:5000 """
+
 from flask import Flask
 
 app = Flask(__name__)
