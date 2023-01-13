f365bfce4756468629ee6c0e404a328a5225804e
cappochinho <edwardprempeh18@gmail.com>
2023-01-13 23:38:12 +0000

    C text

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,5 +11,16 @@ def hello():
     """ Returns a welcome message to 0.0.0.0:5000"""
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    ''' Returns HBNB'''
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ Displays C followed by value in 'text' variable"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
